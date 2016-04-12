ab8df22a99b6af3e9c40ab9732aa91ec877db2bf
Gökhan Şengün <gsengun@netas.com.tr>
2016-04-12 11:34:43 +0300

    Project structure change.

--- BloodPressureTest/RabbitmqSample/RabbitMq/Send.cs
+++ BloodPressureTest/RabbitmqSample/RabbitMq/Send.cs
@@ -17,34 +17,29 @@ namespace TestRabbitMq.RabbitMq
                 Port = 5672
             };
 
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
+            while (true)
             {
-                channel.QueueDeclare(queue: "test",
-                    durable: false,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null);
-
-
-                string message = "Test messages";
-                var body = Encoding.UTF8.GetBytes(message);
-
-
-
-                for (int i = 0; i < 10000; i++)
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
                 {
+                    channel.QueueDeclare(queue: "test",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    string message = "Test messages";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    
                     channel.BasicPublish(exchange: "",
-                   routingKey: "test",
-                   basicProperties: null,
-                   body: body);
-                    Console.WriteLine(" [x] Sent {0} : {1} , Thread{2}, ---- task Id{3}", message,i,
+                            routingKey: "test",
+                            basicProperties: null,
+                            body: body);
+                    Console.WriteLine(" [x] Sent {0} : {1} ---- task Id: {2}", message,
                         System.Threading.Thread.CurrentThread.ManagedThreadId, taskId);
+                
                 }
             }
-
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
         }
 
     }
